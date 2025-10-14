d849f4ad25b80d256b1f1dffd62181be0e4d8036
Abdul Bari <abdulbari80@gmail.com>
2025-10-15 10:28:00 +1100

    codes are modified for log generation

--- src/mlproject/utils/common.py
+++ src/mlproject/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box import BoxValueError
 import yaml
-from mlproject import logger
+from src.mlproject import logging
 import json
 import joblib
 from ensure import ensure_annotations
@@ -28,7 +28,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            logging.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
@@ -62,7 +62,7 @@ def save_json(path: Path, data: dict):
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
 
-    logger.info(f"json file saved at: {path}")
+    logging.info(f"json file saved at: {path}")
 
 
 
@@ -80,7 +80,7 @@ def load_json(path: Path) -> ConfigBox:
     with open(path) as f:
         content = json.load(f)
 
-    logger.info(f"json file loaded succesfully from: {path}")
+    logging.info(f"json file loaded succesfully from: {path}")
     return ConfigBox(content)
 
 
@@ -93,7 +93,7 @@ def save_bin(data: Any, path: Path):
         path (Path): path to binary file
     """
     joblib.dump(value=data, filename=path)
-    logger.info(f"binary file saved at: {path}")
+    logging.info(f"binary file saved at: {path}")
 
 
 @ensure_annotations

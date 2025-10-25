7f7106ecb9d334fcf082ca19410bdc13323fa847
Abdul Bari <abdulbari80@gmail.com>
2025-10-26 00:00:03 +1100

    modifed

--- src/mlproject/utils/common.py
+++ src/mlproject/utils/common.py
@@ -1,16 +1,12 @@
 import os
 import yaml
-import json
-import joblib
 from box.exceptions import BoxValueError
 from box.config_box import ConfigBox
-from ensure import ensure_annotations
 from pathlib import Path
 from typing import Any
 from src.mlproject import logging
 
 
-@ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """This reads and returns yaml file
 
@@ -34,7 +30,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     except Exception as e:
         raise e
     
-@ensure_annotations
+
 def create_directory(path_to_directory: list, verbose=True):
     """This creates a list of directories
 
@@ -50,68 +46,6 @@ def create_directory(path_to_directory: list, verbose=True):
         if verbose:
             logging.info(f"Success! created directory at: {path}")
 
-
-@ensure_annotations
-def save_json(path: Path, data: dict):
-    """This saves json data
-
-    Args:
-        path (Path): path to json file
-        data (dict): data to be saved in json file
-    Returns: 
-        None
-    """
-    with open(path, "w") as f:
-        json.dump(data, f, indent=4)
-
-    logging.info(f"Success! A json file saved at: {path}")
-
-@ensure_annotations
-def load_json(path: Path) -> ConfigBox:
-    """This loads json data
-
-    Args:
-        path (Path): path to json file
-
-    Returns:
-        ConfigBox: data as class attributes instead of dict
-    """
-    with open(path) as f:
-        content = json.load(f)
-
-    logging.info(f"Success! loaded a json file from: {path}")
-    return ConfigBox(content)
-
-
-@ensure_annotations
-def save_bin(data: Any, path: Path):
-    """This saves binary file
-
-    Args:
-        data (Any): data to be saved as binary
-        path (Path): path to binary file
-    Returns: 
-        None
-    """
-    joblib.dump(value=data, filename=path)
-    logging.info(f"A binary file saved at: {path}")
-
-
-@ensure_annotations
-def load_bin(path: Path) -> Any:
-    """This loads binary data
-
-    Args:
-        path (Path): path to binary file
-
-    Returns:
-        Any: object stored in the file
-    """
-    data = joblib.load(path)
-    logging.info(f"Success! binary file loaded from: {path}")
-    return data
-
-@ensure_annotations
 def get_size(path: Path) -> str:
     """This retrieves size in KB
 

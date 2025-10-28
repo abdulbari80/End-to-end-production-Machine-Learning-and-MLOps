ffc98beed43058d0ffa9f5ac176a221775fcafe3
Abdul Bari <abdulbari80@gmail.com>
2025-10-28 11:11:18 +1100

    inor modification

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 # Project metadata
 AUTHOR_NAME = "Abdul Bari"
